feb2f9dbfa5628277932616e888a8dc534edda46
kristjan kangro <kristjan.kangro@visma.com>
2025-02-08 18:48:36 +0200

    module 3 done

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,86 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {CheckingAccount} from "./checkingAccount.ts";
+import {person} from "./Person.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+//2 intro
+{
+// #### Create object using object literal
+	const objLiteral = {
+		balance: 500
+	};
+
+// #### Create object using a class
+	class ClassObject {
+		balance = 1000;
+	}
+
+	const classObj = new ClassObject();
+
+// #### Create object using a function
+	function FunctionObject() {
+		return {balance: 9000}
+	}
+
+	const functionObj = FunctionObject();
+
+// #### Create object using Object.create()
+	const objCreate = Object.create(objLiteral);
+	const total = objLiteral.balance + classObj.balance + functionObj.balance + objCreate.balance;
+
+	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+   <h2>Welcome to Acme Bank!</h2><br /><h5>Your account balances:</h5><br />
+        Object Literal Object Balance: $${objLiteral.balance}
+        <br />
+        Class Object Balance: $${classObj.balance}
+        <br />
+        Function Constructor Object Balance: $${functionObj.balance}
+        <br />
+        Object.create() Object Balance: $${objCreate.balance}
+
+        <br /><br />
+        <strong>Total:</strong> $${total}
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+}
+
+//3 class & object
+
+class Renderer {
+	static render(html: string) {
+		document.querySelector('#app')!.innerHTML = html;
+	}
+}
+
+class Main {
+
+	checkingAccount: CheckingAccount;
+	constructor() {
+		this.checkingAccount = new CheckingAccount("Kristjan Kangro arveldus");
+		this.renderAccount()
+	}
+
+	renderAccount() {
+		console.log('render account');
+		const html = `<h1>${this.checkingAccount.title}</h1>
+Amount: ${this.checkingAccount.balance}
+<input type="number" id="depositWithdrawalAmount" placeholder="deposit"/>
+<button id="deposit" onclick="main.depositWithDrawal()">deposit</button>
+<button id="withdraw" onclick="main.depositWithDrawal()">withdraw</button>
+`;
+		Renderer.render(html)
+	}
+
+	depositWithDrawal() {
+		let amountInput: HTMLInputElement = document.querySelector('#depositWithdrawalAmount')!;
+		let amount = +amountInput.value;
+		console.log(amount);
+			this.checkingAccount.updateBalance(amount);
+		this.renderAccount();
+	}
+}
+
+const main = new Main();
+
+(<any>window).main = main;
+(<any>window).person = person;
